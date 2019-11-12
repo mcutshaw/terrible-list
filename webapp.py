c723f96056ad50328916ca6c1f0148559050a85b
mcutshaw <trash@jackofnotrades.org>
2019-11-11 18:18:42 -0600

    boot2

--- webapp.py
+++ webapp.py
@@ -125,6 +125,6 @@ def remove():
     else:
         print(form.errors)
 
-    return render_template('remove.html', title='Remove')
+    return render_template('remove.html', title='Remove', form=form)
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
